f68451c8864570e59a7b51ceeb851bb5fbc0a940
Daksh Jotwani <dakshjay@gmail.com>
2017-09-24 14:35:35 -0400

    Reduce printing code in LinkedListPartition.java

--- src/main/java/LinkedListPartition.java
+++ src/main/java/LinkedListPartition.java
@@ -1,24 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
-/**
- * Created by daksh on 15-Oct-16.
- */
 public class LinkedListPartition {
-
-    public static void main(String[] args) {
-        Node node = new Node(3);
-        node.next = new Node(5);
-        node.next.next = new Node(8);
-        node.next.next.next = new Node(5);
-        node.next.next.next.next = new Node(10);
-        node.next.next.next.next.next = new Node(2);
-        node.next.next.next.next.next.next = new Node(1);
-
-        LinkedListPartition partition = new LinkedListPartition();
-
-        partition.printList(new LinkedListPartition().partition(node, 5));
-    }
 
     private Node partition(Node node, int x) {
         if (node == null) return null;
@@ -44,21 +24,6 @@ public class LinkedListPartition {
         return head;
     }
 
-    private void printList(Node doNotTouch) {
-        Node node = doNotTouch;
-
-        if (node == null) return;
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        while (node != null) {
-            list.add(node.data);
-            node = node.next;
-        }
-
-        System.out.println(Arrays.toString(list.toArray()));
-    }
-
     private static class Node {
         int data;
         Node next;
@@ -66,5 +31,22 @@ public class LinkedListPartition {
         private Node(int data) {
             this.data = data;
         }
+
+        @Override
+        public String toString() {
+            return String.valueOf(data) + " " + (next != null ? next : "");
+        }
+    }
+
+    public static void main(String[] args) {
+        Node node = new Node(3);
+        node.next = new Node(5);
+        node.next.next = new Node(8);
+        node.next.next.next = new Node(5);
+        node.next.next.next.next = new Node(10);
+        node.next.next.next.next.next = new Node(2);
+        node.next.next.next.next.next.next = new Node(1);
+
+        System.out.println(new LinkedListPartition().partition(node, 5));
     }
 }
