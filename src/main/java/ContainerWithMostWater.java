c0b80274902a2cc6fae62b63a93ac98df0ec77aa
Daksh Jotwani <dakshjay@gmail.com>
2017-11-26 14:46:51 -0500

    Add documentation for ContainerWithMostWater.java

--- src/main/java/ContainerWithMostWater.java
+++ src/main/java/ContainerWithMostWater.java
@@ -9,6 +9,8 @@ class ContainerWithMostWater {
         while (start < end) {
             maxArea = Math.max(maxArea, getArea(heights, start, end));
 
+            // Keep the higher wall still in consideration, and go to the
+            // next index of the smaller wall.
             if (heights[start] < heights[end]) {
                 start++;
             } else {
@@ -20,7 +22,11 @@ class ContainerWithMostWater {
     }
 
     private int getArea(final int[] heights, final int start, final int end) {
+        // Height of the container is the minimum of the two vertical lines,
+        // since water will overflow from the lower wall if we try to fill it more than its height.
         final int height = Math.min(heights[start], heights[end]);
+
+        // Length of the base of the container
         final int width = end - start;
 
         return height * width;
